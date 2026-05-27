da57570c125d1c5ef4188a9c5cfc56498b46eadd
varunsriramoju <varunsriramoju396@gmail.com>
2026-05-27 17:20:52 +0530

    Initial commit

---
+++ shopzone-backend/src/test/java/com/shopzone/shopzone_backend/ShopzoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopzone.shopzone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopzoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
